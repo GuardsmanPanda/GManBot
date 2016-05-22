3cb3ce2748c46edcd0f0e5c6e407f132cb6aceb6
GuardsmanBob <guardsmanpanda@gmail.com>
2016-05-23 01:46:07 +0200

    Test Commit

---
+++ src/main/java/GManBot.java
@@ -0,0 +1,12 @@
+import org.pircbotx.Configuration;
+
+/**
+ * Created by Dons on 23-05-2016.
+ *
+ */
+public class GManBot {
+    public static void main(String[] arguments) {
+        Configuration config = new Configuration.Builder().setName("GManTestBot").buildConfiguration();
+        System.out.println("Its Alive!");
+    }
+}
