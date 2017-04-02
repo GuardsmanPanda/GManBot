e264f7be3627748cc16709cf0d4d0712d1675147
GuardsmanBob <guardsmanpanda@gmail.com>
2017-04-02 19:59:12 +0200

    Now with java9 module support!.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module gmanbot.main {
+    requires jdk.incubator.httpclient;
+    requires guava;
+    requires pircbotx;
+    requires jackson.databind;
+    requires jackson.core;
+    requires jackson.annotations;
+    requires commons.lang;
+    requires java.desktop;
+    requires httpclient;
+    requires httpcore;
+    requires jnativehook;
+    requires java.logging;
+    requires java.sql.rowset;
+    requires yamlbeans;
+    requires javafx.graphics;
+    requires twitter4j.core;
+    requires jdk.httpserver;
+}
